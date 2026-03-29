d7b219a3083cd2bfbd33309acf34c620068fde95
ChristianDavSS <cdavidsanchez054@gmail.com>
2026-03-29 16:06:47 -0600

    basic Excel modifying (test and bugfix required)

--- domain/billing_parser.py
+++ domain/billing_parser.py
@@ -3,23 +3,32 @@ import pandas as pd
 
 class BillingParser:
     def __init__(self, dollar_price: float):
+        # Current dollar price
         self.dollar_price = dollar_price
+        # Code-Price relation (USD)
         self.prices = {'010-CCW': 250, '008-CCS': 150, '009-CCP': 150, '001-FPI': 38, '002-FPC': 36, '003-FPP': 80, '004-WWS': 42, '005-INS': 21,
           '006-LSH': 50, '007-LSG': 26, '011-LIS': 35, '012-PCT': 75, '013-SPC': 35, '014-MIG': 10, '015-RPP': 80, '016-SAL': 35,
           '017-IRP': 35, '018-LIS': 75, '019-IRL': 35, '020-PWT': 30, '002-FPC': 36, '001-GAP': 13, '002-GAP': 14, '021-AGT': 25,
           'TFM-001': 80, 'TFM-003': 22, 'TFM-008': 32, 'TFM-009': 21, 'TFM-010': 38, 'TFM-011': 22, 'TFM-012': 495, 'TFM-013': 21,
           'TFM-014': 36, 'GIP-001': 3000 / self.dollar_price, 'CSS-001': 100, '003-GAP': 23, '021-LS6': 85, 'TFM-015': 160, 'TPA-015': 28}
+        # Instanciate the file manager
         self.file_manager = File()
         
         
     def parse(self):
-        generals = self.parse_generals()
-        totals = self.parse_totals()
-    
-    
-    def parse_generals(self) -> pd.DataFrame:
         """
-        Parase
+        Entry Point: Executes the file parsing and modifying.
+        """
+        # Parse the sheets
+        generals_merged, generals = self.parse_generals()
+        totals = self.parse_totals()
+        # Modify the Excel file
+        self.modify_general(generals_merged, generals)
+    
+    
+    def parse_generals(self) -> tuple[pd.DataFrame, pd.DataFrame]:
+        """
+        Parse the 'GENERALES' sheet to modify it.
         """
         # Read the dataframe
         df = self.file_manager.open_sheet("GENERALES")
@@ -49,17 +58,36 @@ class BillingParser:
             # Concat the dataframes
             merged = pd.concat([merged, data])
         
-        return merged
+        return merged, df
     
     
-    def modify_general(self, merged: pd.DataFrame):
+    def modify_general(self, merged: pd.DataFrame, original: pd.DataFrame):
         """
         Usage of openpyxl to modify the original Excel file
         in-place using indices and absed on the clean dataframe
         """
-        pass
+        # Read the worksheet
+        ws = self.file_manager.load_worksheet("GENERALES")
+        # Iterate the indices
+        for idx in merged.index:
+            # Get the package code
+            code = str(original.iloc[idx, 11]).strip()
+            # Set the dollar price
+            ws.cell(row=idx + 2, column=14, value=self.dollar_price)
+            # Set the code price
+            ws.cell(row= idx + 2, column=15, value=self.prices[code])
+            # Set the price in MXN
+            ws.cell(row = idx + 2, column= 16, value=f"{self.dollar_price * self.prices[code]:,.2f}")
+        
+        # Save the changes
+        self.file_manager.workbook.save(self.file_manager.file)
     
     
     def parse_totals(self) -> pd.DataFrame:
+        """
+        Parse the 'TOTALES' sheet to modify it.
+        """
         df = self.file_manager.open_sheet("TOTALES")
         indices = df.dropna().index.to_list()
+        
+        return df
--- domain/file.py
+++ domain/file.py
@@ -8,9 +8,13 @@ File class: Used to manage the files in the system
 class File:
     def __init__(self):
         self.file = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx")])
+        self.workbook = self.load_workbook()
         
     def open_sheet(self, sheet_name: str):
         return pd.read_excel(self.file, sheet_name = sheet_name)
         
     def load_workbook(self):
         return load_workbook(self.file)
+    
+    def load_worksheet(self, sheet_name: str):
+        return self.workbook[sheet_name]
